233e4da12e14b6298cb48e194c2547d7a69c67a4
Arley Souto <macx15@hotmail.com>
2023-08-24 09:49:42 -0300

    usando describe para agrupar os testes

--- src/utils/getNextDays.test.ts
+++ src/utils/getNextDays.test.ts
@@ -1,9 +1,12 @@
 import { getNextDays } from "./getNextDays";
 
-it("should be return the next five days", () => {
-  //aqui onde vai ser executado o texto
-  const days = getNextDays();
-  
-  //faz uma verificação se está funcionando
-  expect(days.length).toBe(5) // aqui tem que retornar os proximos 5 dias
+//describe serve para agrupar os testes
+describe("getNextDays", () => {
+  it("should be return the next five days", () => {
+    //aqui onde vai ser executado o texto
+    const days = getNextDays();
+
+    //faz uma verificação se está funcionando
+    expect(days.length).toBe(5); // aqui tem que retornar os proximos 5 dias
+  });
 });
