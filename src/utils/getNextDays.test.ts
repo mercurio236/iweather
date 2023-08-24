dd5a0eb8a2a1e6c3a3de5b83a8757e2de4234f41
Arley Souto <macx15@hotmail.com>
2023-08-24 09:34:06 -0300

    criando a estrutura de test

--- src/utils/getNextDays.test.ts
+++ src/utils/getNextDays.test.ts
@@ -1,3 +1,9 @@
+import { getNextDays } from "./getNextDays";
+
 test("should be return the next five days", () => {
   //aqui onde vai ser executado o texto
+  const days = getNextDays();
+  
+  //faz uma verificação se está funcionando
+  expect(days.length).toBe(5) // aqui tem que retornar os proximos 5 dias
 });
