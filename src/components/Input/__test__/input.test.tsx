199998787a561cd89b2db94cbf4e18f5b2acb983
Arley Souto <macx15@hotmail.com>
2023-08-24 12:00:09 -0300

    usando o debug no componente na hora de testar

--- src/components/Input/__test__/input.test.tsx
+++ src/components/Input/__test__/input.test.tsx
@@ -2,5 +2,8 @@ import { render } from "@testing-library/react-native";
 import { Input } from "@components/Input";
 
 describe("Component: Input", () => {
-    
+  it("should be render without activity indicator", () => {
+   const {debug} = render(<Input />);
+   debug();
+  });
 });
