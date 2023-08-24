758bed67ccb9b4235ede9f3e1c289ba6e2e68ea2
Arley Souto <macx15@hotmail.com>
2023-08-24 13:34:32 -0300

    toBeCalledWith, função mock e Testando lista vazia

--- src/components/SelectList/__test__/selectList.test.tsx
+++ src/components/SelectList/__test__/selectList.test.tsx
@@ -7,9 +7,26 @@ describe("Component: SelectList", () => {
       { id: "1", name: "Campinas", latitude: 123, longitude: 456 },
       { id: "2", name: "Campo Grande", latitude: 789, longitude: 987 },
     ];
-    render(<SelectList data={data} onChange={() => {}} onPress={() => {}} />);
+
+    const onPress = jest.fn();
+
+    render(<SelectList data={data} onChange={() => {}} onPress={onPress} />);
 
     const selectedCity = screen.getByText(/Campinas/i); //regex para ignorar o case sensitive
     fireEvent.press(selectedCity); //evento para selecionar pressionando
+
+    //expect(onPress).toBeCalledTimes(1); //se ela foi chamada uma vez
+    expect(onPress).toBeCalledWith(data[1]) //o que foi selecionado
   });
+
+  it("not should be show options when data props is empty", () =>{
+    render(<SelectList
+    data={[]}
+    onChange={() =>{}}
+    onPress={() =>{}}
+    />)
+
+    const options = screen.getByTestId('options')
+    expect(options.children).toHaveLength(0)
+  })
 });
