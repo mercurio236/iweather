0adf1a1d4a04144767c49315198e00abe72a8fe9
Arley Souto <macx15@hotmail.com>
2023-08-25 18:55:53 -0300

    teste do componente nextDays

---
+++ src/components/NextDays/__test__/NextDays.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react-native";
+import { NextDays } from "@components/NextDays";
+
+import clearDay from "@assets/clear_day.svg";
+describe("", () => {
+  it("should be render day.", () => {
+    render(
+      <NextDays
+        data={[
+          {
+            day: "18/07",
+            min: "20°c",
+            max: "34°c",
+            icon: clearDay,
+            weather: "Céu limpo",
+          },
+          {
+            day: "19/07",
+            min: "10°c",
+            max: "34°c",
+            icon: clearDay,
+            weather: "Céu limpo",
+          },
+          {
+            day: "20/07",
+            min: "22°c",
+            max: "34°c",
+            icon: clearDay,
+            weather: "Céu limpo",
+          },
+        ]}
+      />
+    );
+
+    expect(screen.getByText("18/07")).toBeTruthy();
+  });
+});
