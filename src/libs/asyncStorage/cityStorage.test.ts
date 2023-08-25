a33e75c061413576046a4ee6105e022eb5003f2a
Arley Souto <macx15@hotmail.com>
2023-08-25 18:08:32 -0300

    ajuste no teste de cityStorage

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -3,6 +3,7 @@ import { getStorageCity } from "./cityStorage";
 describe("Storage: cityStorage", () => {
   it("should be return null when don't have a city storage", async () => {
     const response = await getStorageCity();
+    
     expect(response).toBeNull();
   });
 });
