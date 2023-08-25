ef2938e9c78b8c47287748b3a807c283987844f8
Arley Souto <macx15@hotmail.com>
2023-08-25 18:36:23 -0300

    testando componente day

---
+++ src/components/Day/__test__/Day.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react-native";
+import { Day } from "@components/Day";
+
+import clearDay from "@assets/clear_day.svg";
+
+describe("Component: Day", () => {
+  it("should be render day", () => {
+    render(
+      <Day
+        data={{
+          day: "18/07",
+          min: "30°c",
+          max: "34°c",
+          icon: clearDay,
+          weather: "Céu limpo",
+        }}
+      />
+    );
+  });
+});
