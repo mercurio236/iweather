a6fec1448363a28250bebeabf594b99136d06b01
Arley Souto <macx15@hotmail.com>
2023-08-25 17:08:29 -0300

    testando retorno da api

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -2,7 +2,7 @@ import { api } from "./api";
 import { getCityByNameService } from "./getCityByNameService";
 import { mockCityApiResponse } from "@__tests__/mocks/mockCityApiResponse";
 
-describe("API: getCityByNameService", () => {
+describe("Service: getCityByNameService", () => {
   it("should return city details", async () => {
     
     jest.spyOn(api, "get").mockResolvedValue({ data:mockCityApiResponse });
